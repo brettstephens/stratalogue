cda3a964f12e2409c83584861e3e7eb62ca60514
Devin Singh <devints47@gmail.com>
2016-02-07 18:24:59 +0000

    Initializing models with base fields

--- core/models.py
+++ core/models.py
@@ -1,5 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Role(models.Model):
+	name = models.CharField(max_length=32)
+
+
+class Map(models.Model):
+	name = models.CharField(max_length=32)
+
+
+class Playbook(models.Model):
+	name = models.CharField(max_length=64)
+	creator = models.OneToOneField(User)
+	private = models.BooleanField(default=True)
+	de_map = models.ForeignKey(Map)
+
+	# Roles
+	role1 = models.ForeignKey(Role, related_name='role1')
+	role2 = models.ForeignKey(Role, related_name='role2')
+	role3 = models.ForeignKey(Role, related_name='role3')
+	role4 = models.ForeignKey(Role, related_name='role4')
+	role5 = models.ForeignKey(Role, related_name='role5')
