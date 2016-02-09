0d0d6ac53e1eb53617aaf7f8eea67fc282958af3
Devin Singh <devints47@gmail.com>
2016-02-09 02:35:36 +0000

    Client-side validation of registration page complete

--- core/static/js/core.js
+++ core/static/js/core.js
@@ -6,29 +6,61 @@ $(document).ready(function() {
 
 		$.each(input_ids, function(index, value) {
 			if ( !$(value).val() ) {
-				invalid_ids += value;
+				$(value).addClass('validation-error');
+				$('.error-list').append('<li>' + $(value).attr('name') + ' field required.</li>');
 				validation_error = 1;
 			}
-			else if (validate_email)
+			else {
+				$(value).removeClass('validation-error');
+			}
 		});
-		return [invalid_ids, validation_error];
+		return validation_error;
 	}
 
 	function validate_email(email_ids) {
 		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		return regex.test($(email_id).val());
+		return regex.test($(email_ids).val());
+	}
+
+	function validate_passwords(password1, password2) {
+		var password_error = 0;
+
+		if (password1.val() !== password2.val()) {
+			$(password1).addClass('validation-error');
+			$(password2).addClass('validation-error');
+			$('.error-list').append('<li>Password fields must match.</li>');
+			password_error = 1;
+		}
+		if (($(password1).val()).length < 6) {
+			$(password1).addClass('validation-error');
+			$(password2).addClass('validation-error');
+			$('.error-list').append('<li>Password must be greater than 6 characters</li>');
+			password_error = 1;
+		}
+
+		return password_error;
 	}
 
 	$('#registration_submit').click(function() {
 		var input_ids = [$('#username'), $('#email'), $('#password1'), $('#password2')];
-		var validate_results = validate_entry(input_ids);
+		$('.error-list').empty();
 
-		if (validate_results[1]) {
-			var invalid_ids = validate_results[0];
+		var validation_error = validate_entry(input_ids);
+		var password_error = validate_passwords($('#password1'), $('#password2'));
 
+
+		if (!(validate_email($('#email')))) {
+			$('.message-box').show();
+			$('.error-list').append('<li>Enter a valid email address</li>');
+			$('#email').addClass('validation-error');
+			var email_error = 1;
+		}
+
+		if (validation_error || email_error || password_error) {
+			$('.message-box').show();
+			return false;
 		}
 		
-
 
 	})
 });
