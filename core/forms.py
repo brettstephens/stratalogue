a270a3f5213660554c2524d35d4ec6d4787f79f5
Devin Singh <devints47@gmail.com>
2016-02-09 06:58:21 +0000

    Added Login form

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,8 @@ class RegistrationForm(forms.Form):
 	email = forms.EmailField(label="email", max_length=64)
 	password1 = forms.CharField(widget=forms.PasswordInput())
 	password2 = forms.CharField(widget=forms.PasswordInput())
+
+
+class LoginForm(forms.Form):
+	username = forms.CharField(label="username", max_length=25)
+	password = forms.CharField(widget=forms.PasswordInput())
